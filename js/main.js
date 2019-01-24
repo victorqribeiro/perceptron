a24fd8314689c390494a3323d01d530ae06c393b
Victor Ribeiro <victorqribeiro@gmail.com>
2019-01-24 18:08:06 -0200

    Fix naming

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-p = new Perceptron(x.length);
+const p = new Perceptron(x.length);
 
 p.fit(x,y);
 
-d = document.createElement('div');
+let d = document.createElement('div');
 
 d.innerText = p.predict([5.2,2.1,4.5,1.5]);
 
